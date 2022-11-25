347987f2bad83aae8259006cd37f17c0149b438a
anjiboss <nhu.nhu.face@gmail.com>
2022-11-25 09:11:06 +0900

    add eletron front-end

---
+++ desktop/src/Call.tsx
@@ -0,0 +1,130 @@
+import React, { useEffect, useMemo, useRef } from "react";
+import { toast } from "react-toastify";
+import { socket } from "./App";
+
+interface CallProps {
+  children?: React.ReactNode;
+  user: {
+    username: string;
+    peer: RTCPeerConnection;
+  };
+  username: string;
+}
+
+const Call: React.FC<CallProps> = ({ user, username }) => {
+  // const remoteMedia = useMemo(() => new MediaStream(), []);
+  const remoteRef = useRef<HTMLVideoElement>(null);
+  const queuedAnswer = useRef<RTCIceCandidate[]>([]);
+
+  const localConnection = useMemo(() => {
+    console.log("create connection");
+    const pc = new RTCPeerConnection({
+      iceServers: [
+        { urls: import.meta.env.VITE_STUNSERVER_1 },
+        {
+          urls: import.meta.env.VITE_STUNSERVER_2,
+          username: import.meta.env.VITE_XIRSYS_USERNAME,
+          credential: import.meta.env.VITE_XIRSYS_CREDENTIAL,
+        },
+      ],
+    });
+    pc.ontrack = (e) => {
+      console.log("%c On Track", "background: blue; color:white");
+      if (e.streams && e.streams[0]) {
+        if (remoteRef.current) {
+          remoteRef.current.srcObject = e.streams[0];
+        }
+      }
+    };
+    pc.addTransceiver("video", { direction: "recvonly" });
+    return pc;
+  }, [user]);
+
+  const createOffer = async () => {
+    const offer = await localConnection.createOffer({
+      offerToReceiveVideo: true,
+      offerToReceiveAudio: true,
+    });
+    await localConnection.setLocalDescription(offer);
+    return offer;
+  };
+
+  useEffect(() => {
+    createOffer().then((offer) => {
+      socket.emit("consumer", { offer, username, streamName: user.username });
+    });
+    socket.on("consumer-answer", ({ answer, to, streamName }) => {
+      if (to === username && streamName === user.username) {
+        console.log(
+          "%c comsumber have answer",
+          "background: grey; color:white"
+        );
+        localConnection
+          .setRemoteDescription(new RTCSessionDescription(answer))
+          .then(() => {
+            if (queuedAnswer.current.length > 0) {
+              queuedAnswer.current.forEach((a) => {
+                localConnection.addIceCandidate(new RTCIceCandidate(a));
+              });
+            }
+          });
+      }
+    });
+
+    localConnection.onicecandidate = (event) => {
+      console.log("send offer candidate to ", user.username);
+      if (event.candidate) {
+        socket.emit("consumer-add-offer-candidate", {
+          candidate: event.candidate,
+          username,
+          streamName: user.username,
+        });
+      }
+    };
+
+    socket.on(
+      "consumer-add-answer-candidate",
+      ({ candidate, to, streamName }) => {
+        if (to === username && streamName === user.username) {
+          console.log(
+            "%c had answer candidate",
+            "background: green; color:white"
+          );
+          if (localConnection.currentLocalDescription) {
+            localConnection.addIceCandidate(new RTCIceCandidate(candidate));
+          } else {
+            queuedAnswer.current.push(candidate);
+          }
+        }
+      }
+    );
+
+    localConnection.onconnectionstatechange = () => {
+      console.log(
+        "%c Connection State: " + localConnection.connectionState,
+        "background: skyblue; color:black"
+      );
+      const connectionState = localConnection.connectionState;
+
+      if (["disconnected", "failed"].includes(connectionState)) {
+        toast(connectionState, { type: "error" });
+      }
+      if (["connected"].includes(connectionState)) {
+        toast(connectionState, { type: "success" });
+      }
+    };
+
+    return () => {
+      socket.off("consumer-answer");
+      socket.off("consumer-add-answer-candidate");
+    };
+  }, [username]);
+
+  return (
+    <span>
+      <video ref={remoteRef} autoPlay></video>
+      <h3>User: {user.username}</h3>
+    </span>
+  );
+};
+export default Call;
